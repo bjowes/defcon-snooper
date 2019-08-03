bd53952352c38c602728857bd9a51b46fe9034ee
Björn Weström <bjowes@gmail.com>
2019-08-03 23:44:04 +0200

    with logging

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
 
-const wifiScanInterval = 1 * 60 * 1000;
+const wifiScanInterval = 15 * 1000;
 const wifiScan = require('./wifi-scan-parse');
 
 setInterval(() => {
     console.log('Scanning Wifi networks');
     wifiScan.scanWifiNetworksAndInsert();
+    wifiScan.getWifisFromDb();
 }, wifiScanInterval);
 
 
