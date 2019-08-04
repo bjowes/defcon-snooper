f7202296cc70197ec5ee51c0255b29e646c2f5e3
Björn Weström <bjowes@gmail.com>
2019-08-04 19:46:25 +0200

    updated to sync dns lookup

--- result-push.js
+++ result-push.js
@@ -1,6 +1,6 @@
 const debug = require('debug')('defcon-snoop');
 const { execSync } = require('child_process');
-const dns = require('dns');
+const dnsSync = require('dns-sync');
 
 function activateWifi() {
 
@@ -10,16 +10,15 @@ function disableWifi() {
 
 }
 
-function connected() {
-    dns.resolve('www.google.com', function(err) {
-        if (err) {
-           console.log("No connection");
-           return false;
-        } else {
-           console.log("Connected to internet");
-           return true;
-        }
-    });
+function connected(fn) {
+    let resolved = dnsSync.resolve('www.google.com');
+    if (!resolved) {
+        console.log("No connection");
+        return false;
+    } else {
+        console.log("Connected to internet");
+        return true;
+    }
 }
 
 function uploadToDropbox() {
